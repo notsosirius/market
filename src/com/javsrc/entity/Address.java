c821fe1ab396501e82685e3d62f1c6411f6bf002
notsosirius <49362413+notsosirius@users.noreply.github.com>
2020-11-30 19:27:04 +0800

    Update Address.java

--- src/com/javsrc/entity/Address.java
+++ src/com/javsrc/entity/Address.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 /**
  * 会员的地址
- * @author qiujy
  */
 public class Address implements Serializable {
 	private static final long serialVersionUID = 529932754573978762L;
